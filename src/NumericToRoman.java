c877b4b911e5d2f188a63f7146f14e31f7401af0
FeesTheFox <feesblock@gmail.com>
2024-03-28 00:14:29 +0300

    Succeeded in returning Numeric to Roman in LeetCode

---
+++ src/NumericToRoman.java
@@ -0,0 +1,24 @@
+public class NumericToRoman {
+    public String intToRoman(int num) {
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thousands = {"","M","MM","MMM"};
+
+        StringBuilder ans = new StringBuilder();
+
+        ans.append(thousands[num / 1000]);
+        ans.append(hundreds[(num % 1000) / 100]);
+        ans.append(tens[(num % 100) / 10]);
+        ans.append(ones[num % 10]);
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        NumericToRoman toRoman = new NumericToRoman();
+        int num = 1333;
+        String res = toRoman.intToRoman(num);
+        System.out.println(res);
+    }
+}
